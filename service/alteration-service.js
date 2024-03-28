70fcbb59b9551757663ed81e0ef4c9502d46e3e6
Manuel <manuelmaterazzo@gmail.com>
2024-03-28 22:40:46 +0100

    jsdocs

--- service/alteration-service.js
+++ service/alteration-service.js
@@ -2,6 +2,12 @@ import converter from "rel-to-abs";
 
 export default class AlterationService {
 
+    /**
+     * Extracts the originalResponse headers, and adds/replaces the ones passed
+     * @param originalResponse response to extract base headers
+     * @param headers headers to add to the base headers
+     * @returns {*} header object
+     */
     static getResponseHeaders(originalResponse, headers) {
         // add all headers of the original response but content-encoding. gzip is trouble.
         const responseHeaders = {...originalResponse.headers};
@@ -19,6 +25,13 @@ export default class AlterationService {
         return responseHeaders;
     }
 
+    /**
+     * Replaces all URLs on the response with the provided url
+     * @param originalResponse response to edit
+     * @param oldUrl url to search (auto-discovers relative urls)
+     * @param newUrl url to replace oldUrl with
+     * @returns {Promise<string>} String containing the edited body
+     */
     static async rewriteUrls(originalResponse, oldUrl, newUrl) {
         return converter.convert(await originalResponse.text(), oldUrl)
                     .replace(oldUrl, newUrl);
--- service/rest-service.js
+++ service/rest-service.js
@@ -3,6 +3,14 @@ import AlterationService from "./alteration-service.js";
 import {Readable} from "stream"
 
 export default class RestService {
+
+    /**
+     * Fetches the provided URL by applying the requested edits
+     * @param requestedUrl url to fetch
+     * @param requestEdits edits applied to the request to be made
+     * @param responseEdits edits applied to the fetch response
+     * @returns {Promise<Readable|NodeJS.ReadableStream>} edited fetch response
+     */
     static async fetchAndEdit(requestedUrl, requestEdits, responseEdits) {
         return await fetch(requestedUrl, {
             method: requestEdits.method ?? 'GET',
@@ -20,6 +28,12 @@ export default class RestService {
         });
     }
 
+    /**
+     * Converts a string to a express-compatible ReadableStream
+     * @param value string to be converted
+     * @returns {Readable} express-compatible ReadableStream
+     * @private
+     */
     static _toReadableStream(value) {
         return Readable.from(value);
     }
