e0e82647dcac3a8a82067a893e74ba0eb5a14b4d
Manuel <manuelmaterazzo@gmail.com>
2026-03-10 00:20:35 +0100

    Fix: Unhandled async errors crash workers

--- controller/bootstrap.js
+++ controller/bootstrap.js
@@ -1,6 +1,5 @@
 'use strict';
 
-import fs from "fs";
 import RestService from "../service/rest-service.js"
 import QueryparamService from "../service/queryparam-service.js";
 import PathvariableService from "../service/pathvariable-service.js";
@@ -26,80 +25,103 @@ export default app => {
         res.send(requestGenerator);
     });
 
-    app.all('/header', async (req, res) => {
-        const baseUrl = '//' + req.get('host');
+    app.all('/header', async (req, res, next) => {
+        try {
+            const baseUrl = '//' + req.get('host');
 
-        const requestEdits = HeaderService.extractRequestEdits(req.headers, req.method);
-        const responseEdits = HeaderService.extractResponseEdits(baseUrl, req.headers);
-        const errorEdits = HeaderService.extractErrorEdits(req.headers);
+            const requestEdits = HeaderService.extractRequestEdits(req.headers, req.method);
+            const responseEdits = HeaderService.extractResponseEdits(baseUrl, req.headers);
+            const errorEdits = HeaderService.extractErrorEdits(req.headers);
 
-        const requestedUrl = requestEdits.url;
-
-        const response = await RestService.fetchAndEdit(requestedUrl, requestEdits, responseEdits, errorEdits);
-
-        res.set(response.headers);
-        res.status(response.status);
-        response.body.pipe(res);
-    });
-
-    app.all('/queryparam', async (req, res) => {
-        const requestedUrl = req.query.url
-        const baseUrl = '//' + req.get('host');
-
-        const requestEdits = QueryparamService.extractRequestEdits(req.query, req.method);
-        const responseEdits = QueryparamService.extractResponseEdits(baseUrl, req.query);
-        const errorEdits = QueryparamService.extractErrorEdits(req.query);
+            const requestedUrl = requestEdits.url;
 
         const response = await RestService.fetchAndEdit(requestedUrl, requestEdits, responseEdits, errorEdits);
 
-        res.set(response.headers);
-        res.status(response.status);
-        response.body.pipe(res);
-    });
-
-
-    app.all('/path-variable/*', async (req, res) => {
-
-        // slice away the "/path-variable/" part and parse path variables
-        const edits = PathvariableService.extractEdits(req.url.slice(15));
-        const requestedUrl = edits.url;
-
-        const responseEdits = {
-            baseUrl: '//' + req.get('host'),
-            headers: {},
-            body: {
-                rewriteUrls: edits.rewrite,
+            res.set(response.headers);
+            res.status(response.status);
+            if (response.body) {
+                response.body.pipe(res);
+            } else {
+                res.end();
             }
-        };
-
-        const errorEdits = {
-            alwaysok: false,
-            responseType: "text"
+        } catch (err) {
+            next(err);
         }
+    });
+
+    app.all('/queryparam', async (req, res, next) => {
+        try {
+            const requestedUrl = req.query.url
+            const baseUrl = '//' + req.get('host');
+
+            const requestEdits = QueryparamService.extractRequestEdits(req.query, req.method);
+            const responseEdits = QueryparamService.extractResponseEdits(baseUrl, req.query);
+            const errorEdits = QueryparamService.extractErrorEdits(req.query);
+
+        const response = await RestService.fetchAndEdit(requestedUrl, requestEdits, responseEdits, errorEdits);
+
+            res.set(response.headers);
+            res.status(response.status);
+            if (response.body) {
+                response.body.pipe(res);
+            } else {
+                res.end();
+            }
+        } catch (err) {
+            next(err);
+        }
+    });
+
+
+    app.all('/path-variable/*', async (req, res, next) => {
+        try {
+            // slice away the "/path-variable/" part and parse path variables
+            const edits = PathvariableService.extractEdits(req.url.slice(15));
+            const requestedUrl = edits.url;
+
+            const responseEdits = {
+                baseUrl: '//' + req.get('host'),
+                headers: {},
+                body: {
+                    rewriteUrls: edits.rewrite,
+                }
+            };
+
+            const errorEdits = {
+                alwaysok: false,
+                responseType: "text"
+            }
 
         const response = await RestService.fetchAndEdit(requestedUrl, edits, responseEdits, errorEdits);
 
-        res.set(response.headers);
-        res.status(response.status);
-        response.body.pipe(res);
+            res.set(response.headers);
+            res.status(response.status);
+            if (response.body) {
+                response.body.pipe(res);
+            } else {
+                res.end();
+            }
+        } catch (err) {
+            next(err);
+        }
     });
 
     // fallback for urls it fails to rewrite, just rethrow the request to another controller
-    app.all('/*', async (req, res) => {
-
-        const referer = req.headers['referer'] ?? '';
+    app.all('/*', async (req, res, next) => {
+        try {
+            const referer = req.headers['referer'] ?? '';
 
         let requestedUri = req.url;
 
-        // slice away the initial "/", if it gets added by the referer
-        if (referer.endsWith('/')) {
-            requestedUri = requestedUri.slice(1);
-        }
+            // slice away the initial "/", if it gets added by the referer
+            if (referer.endsWith('/')) {
+                requestedUri = requestedUri.slice(1);
+            }
 
-        // add the uri to the referer url, it should produce a valid proxied request
-        const requestedUrl = referer + requestedUri;
+            // add the uri to the referer url, it should produce a valid proxied request
+            const requestedUrl = referer + requestedUri;
 
-        console.info("Got an invalid request for: " + requestedUri + " , falling back to: " + requestedUrl);
+            console.info("Got an invalid request for: " + requestedUri + " , falling back to: " + requestedUrl);
 
         const requestEdits = {
             url: requestedUrl,
@@ -108,23 +130,30 @@ export default app => {
             method: 'GET',
         }
 
-        const responseEdits = {
-            baseUrl: '//' + req.get('host'),
-            headers: {},
-            body: {
-                rewriteUrls: false,
+            const responseEdits = {
+                baseUrl: '//' + req.get('host'),
+                headers: {},
+                body: {
+                    rewriteUrls: false,
+                }
+            };
+
+            const errorEdits = {
+                alwaysok: false,
+                responseType: "text"
             }
-        };
 
-        const errorEdits = {
-            alwaysok: false,
-            responseType: "text"
+            const response = await RestService.fetchAndEdit(requestedUrl, requestEdits, responseEdits, errorEdits);
+
+            res.set(response.headers);
+            res.status(response.status);
+            if (response.body) {
+                response.body.pipe(res);
+            } else {
+                res.end();
+            }
+        } catch (err) {
+            next(err);
         }
-
-        const response = await RestService.fetchAndEdit(requestedUrl, requestEdits, responseEdits, errorEdits);
-
-        res.set(response.headers);
-        res.status(response.status);
-        response.body.pipe(res);
     });
 };
